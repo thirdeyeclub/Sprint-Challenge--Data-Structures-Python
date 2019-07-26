8848051e885a7fce18ef3468ddfc17a171305471
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-26 12:15:35 -0400

    rewrte ringbffer with tests!

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,17 +6,30 @@ class RingBuffer:
 
   def append(self, item):
     print("current value:",self.current)
+    print("store:", self.storage)
     self.storage[self.current] = item
     if self.current < self.capacity -1:
-      self.current += 1
-    else:
+      self.current += 1 
+    else: 
       self.current = 0
-    #should reset it
+      self.append(item)
 
   def get(self):
     request = []
+    
     for i in self.storage:
       if i is not None:
         request.append(i)
-      return request
-      print(request)
+    return request
+
+
+
+#tests
+buffer = RingBuffer(3)
+
+print(buffer.get())   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+print("Get =>",buffer.get())
